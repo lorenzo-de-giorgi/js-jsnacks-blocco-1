b39e117ab8aee46b93e07c8928db3b22f810d0a9
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-12 13:36:34 +0100

    array-lista completed

---
+++ array-lista/script.js
@@ -0,0 +1,8 @@
+let fridge = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+let pesca = fridge.push('pesca');
+
+if (fridge.indexOf('cocomero') !== -1) {
+    console.log('Cocomero presente');
+} else {
+    console.log('Cocomero non presente');
+}
